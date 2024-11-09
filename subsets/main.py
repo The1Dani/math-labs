2359cd226fe9abffcb9eaa968b1e10d04976329e
The1Dani <daniel.canter2013@gmail.com>
2024-11-09 22:28:01 +0200

    First commit to my math labs

---
+++ subsets/main.py
@@ -0,0 +1,50 @@
+
+def main():
+    myset = []
+
+    get_set(myset)
+
+    # print_set(myset)
+
+    myset = set(myset)
+
+    pset = get_subsets(myset)
+    
+
+    print_set(pset)
+
+
+def get_subsets(input_set):
+    
+    result = [[]]  # Start with the empty set
+
+    for element in input_set:
+        # Create new subsets by adding the current element to each existing subset
+        # for subset in result:
+        #     print(subset + [element])
+        result += [subset + [element] for subset in result]
+    
+    return result
+            
+
+
+
+
+def get_set(s):
+
+    inp = input("set = ")
+
+    inp = inp.replace(" ", "").split(",")
+
+    for element in inp:
+        s.append(element)
+    
+
+def print_set(s):
+    print("[", end="")
+    print(*s, end="", sep=",")
+    print("]", end="")
+    print()
+
+if __name__ == "__main__":
+    main()
---
+++ xnor/main.py
@@ -0,0 +1,36 @@
+
+bools = ["0", "1", "True", "False"]
+
+def main():
+    a = get_input("First")
+    b = get_input("Second")
+
+    result = xnor(a, b)
+
+    print(result)
+
+def is_zero(x):
+    try:
+        return int(x) == 0
+    except:
+        return 0
+
+def transform_bool(x):
+    return False if is_zero(x) == True or x.capitalize() == "False" else True
+    
+
+def get_input(nth):
+    
+    while True:
+        a = input(f"{nth} boolean expression: ")
+        if a.capitalize() in bools:
+            a = transform_bool(a)
+            break
+    return a
+
+def xnor(a, b):
+    return (a and b) or (not a and not b) 
+
+
+if __name__ == "__main__":
+    main()
